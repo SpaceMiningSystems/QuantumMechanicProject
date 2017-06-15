5913f3680cd506b0dcef2131cae964955210cfec
SpaceMiningSystems <mark@spaceminingsystems.com>
2017-06-14 21:12:10 -0400

    Continuing

--- Assets/Scripts/ScriptBasicProperties.cs
+++ Assets/Scripts/ScriptBasicProperties.cs
@@ -9,40 +9,16 @@ namespace MarkNameSpace
 
         public bool perfectMagnet;
 
-        // Use this for initialization
-        void Start () {
-            
-        }
-	
-		// Update is called once per frame
-		void Update () {
-            
-		}
-
-		void OnEnable()
-		{
-
-		}
-
-		void OnDisable()
-		{
-
-		}
-
-		void SetInitialReferences()
-		{
-
-		}
-
-        static int collideCount = 0;
-
         void OnCollisionEnter(Collision hit)
         {
-
             
+
             if (hit.collider.tag != "Floor" && hit.collider.tag != "Player")
             {
 
+                GetComponent<Rigidbody>().detectCollisions = false;
+                hit.collider.GetComponent<Rigidbody>().detectCollisions = false;
+                StartCoroutine(DelayedKinematic());
                 this.transform.rotation = hit.transform.rotation;
 
                 if(perfectMagnet)
@@ -61,74 +37,14 @@ namespace MarkNameSpace
 
             }
 
-            //foreach (ContactPoint contact in hit.contacts)
-            //{
-            //    if(contact.otherCollider.tag != "Floor" && contact.otherCollider.tag != "Player")
-            //    {
-            //        //this.transform.position =  new Vector3 contact.otherCollider.transform.position;
-
-            //        this.transform.rotation = hit.transform.rotation;
-            //        this.transform.position = hit.transform.position + new Vector3 (0,0,1);
-
-            //        //if (otherCube != null)
-            //        //{
-            //        //    this.transform.rotation = otherCube.transform.rotation;
-            //        //    this.transform.position = otherCube.transform.position + new Vector3(0, 0, 1);
-            //        //}
-
-            //        FixedJoint fixedJoint = gameObject.AddComponent<FixedJoint>();
-            //        fixedJoint.anchor = contact.point;
-            //        fixedJoint.connectedBody = hit.rigidbody;
-
-
-
-
-            //        //this.transform.parent = contact.otherCollider.gameObject.transform;
-            //        //transform.SetParent(contact.otherCollider.gameObject.transform);
-            //        //Debug.Log("My parent is now " + contact.otherCollider.gameObject.transform);
-                    
-            //    }          
-            //}
+            
         }
 
-
-        void TOnCollisionEnter(Collision collision)
+        IEnumerator DelayedKinematic()
         {
-            if (collision.collider.tag != "Floor" && collision.collider.tag != "Player")
-            {
-                Debug.Log(ReturnDirection(collision.gameObject, this.gameObject));
-            }
-                
-        }
+            yield return new WaitForSeconds(0.1f);
 
-        private enum HitDirection { None, Top, Bottom, Forward, Back, Left, Right }
-        private HitDirection ReturnDirection(GameObject Object, GameObject ObjectHit)
-        {
 
-            HitDirection hitDirection = HitDirection.None;
-            RaycastHit MyRayHit;
-            //Vector3 direction = (Object.transform.position - ObjectHit.transform.position).normalized;
-            Vector3 direction = (ObjectHit.transform.position - Object.transform.position).normalized;
-            Ray MyRay = new Ray(ObjectHit.transform.position, direction);
-
-            if (Physics.Raycast(MyRay, out MyRayHit))
-            {
-
-                if (MyRayHit.collider != null)
-                {
-
-                    Vector3 MyNormal = MyRayHit.normal;
-                    MyNormal = MyRayHit.transform.TransformDirection(MyNormal);
-
-                    if (MyNormal == MyRayHit.transform.up) { hitDirection = HitDirection.Top; }
-                    if (MyNormal == -MyRayHit.transform.up) { hitDirection = HitDirection.Bottom; }
-                    if (MyNormal == MyRayHit.transform.forward) { hitDirection = HitDirection.Forward; }
-                    if (MyNormal == -MyRayHit.transform.forward) { hitDirection = HitDirection.Back; }
-                    if (MyNormal == MyRayHit.transform.right) { hitDirection = HitDirection.Right; }
-                    if (MyNormal == -MyRayHit.transform.right) { hitDirection = HitDirection.Left; }
-                }
-            }
-            return hitDirection;
         }
 
     }
